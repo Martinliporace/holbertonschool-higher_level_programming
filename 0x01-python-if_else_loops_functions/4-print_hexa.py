ea60b033151b4455b7d1915e66b148663c296953
root <martin.liporace@gmail.com>
2022-04-26 10:21:44 -0700

    4-print_hexa.py fxd

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,22 +1,3 @@
 #!/usr/bin/python3
-num = 0
-ciclo = 0
-
 for i in range(0, 99):
-    resto = i % 16
-    if resto <= 15 and resto >= 10 and i <= 15:
-        print(f"{i} = 0x{chr(resto + 87)}")
-        if i % 15 == 0:
-            ciclo += 1
-
-    elif resto <= 15 and resto >= 10 and i > 15:
-        div = i // 16
-        print(f"{i} = 0x{div}{chr(resto + 87)}")
-        if i % 15 == 0:
-            ciclo += 1
-
-    else:
-        x = ciclo * 6
-        print(f"{i} = 0x{i - x}")
-
-    num += 1
+    print("{:c}.format(i) = {hex(i)}")
