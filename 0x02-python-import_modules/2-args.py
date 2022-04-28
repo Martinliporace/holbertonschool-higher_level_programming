05d4aaced1967484e5a8753ecc4b8491543e280d
root <martin.liporace@gmail.com>
2022-04-28 09:38:17 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+
+    args = sys.argv
+
+    cant = len(args)
+
+    if cant == 1:
+        print(cant - 1, "arguments.")
+    elif cant == 2:
+        print(cant - 1, "argument:", args[1])
+
+    else:
+        print(cant - 1, "arguments:")
+        cont = 1
+        for i in range(1, cant):
+            print(cont, ":", args[i])
+            cont += 1
