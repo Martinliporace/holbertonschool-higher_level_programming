3268e60f8ae47489b47fd4aeda7482eb6023d97f
root <martin.liporace@gmail.com>
2022-05-02 11:41:14 -0700

    7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    cpa = [0, 0]
+    cpb = [0, 0]
+
+    for i in range(len(tuple_a)):
+        cpa[i] = tuple_a[i]
+    for j in range(len(tuple_b)):
+        cpb[j] = tuple_b[j]
+
+    return(cpa[0] + cpb[0], cpa[1] + cpb[1])
