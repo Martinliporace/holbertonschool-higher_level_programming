27485aee26142b399c0fa966e0e37a4ab07aebbb
root <martin.liporace@gmail.com>
2022-05-04 09:55:40 -0700

    12-roman_to_int.py refxd non valid chars again

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,22 +3,16 @@ def roman_to_int(roman_string):
 
     values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     aux = []
-
     for i in roman_string:
-        if i in values:
-            aux.append(values[i])
-        else:
-            return(0)
+        aux.append(values[i])
 
     aux.append(0)
 
-    res = 0
-    for j in range(0, len(aux) - 1):
+    res = aux[0]
+    for j in range(1, len(aux) - 1):
 
         if aux[j + 1] > aux[j]:
-            res = res - aux[j]
-
+            res -= aux[j]
         else:
             res += aux[j]
-            
     return(res)
