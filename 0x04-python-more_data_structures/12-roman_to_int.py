56d38241f74307ffbb36c0a2b738f3753c835c4f
root <martin.liporace@gmail.com>
2022-05-04 06:47:46 -0700

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+
+    values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    aux = []
+    for i in roman_string:
+        aux.append(values[i])
+
+    aux.append(0)
+
+    res = aux[0]
+    for j in range(1, len(aux) - 1):
+
+        if aux[j + 1] > aux[j]:
+            res -= aux[j]
+        else:
+            res += aux[j]
+    return(res)
