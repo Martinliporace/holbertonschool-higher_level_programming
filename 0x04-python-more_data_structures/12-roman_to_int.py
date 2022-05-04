0783d8f475ba759da2871b8007802de498de7784
root <martin.liporace@gmail.com>
2022-05-04 09:31:55 -0700

    12-roman_to_int.py fxd non valid chars

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,10 @@ def roman_to_int(roman_string):
 
     values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     aux = []
+
     for i in roman_string:
-        aux.append(values[i])
+        while i is in values:
+            aux.append(values[i])
 
     aux.append(0)
 
