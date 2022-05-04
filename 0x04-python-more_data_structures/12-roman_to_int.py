6c413e8880e1623ce3d1106919d511487e589305
root <martin.liporace@gmail.com>
2022-05-04 09:57:36 -0700

    12-roman_to_int.py vamo de nuevo

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,16 +3,18 @@ def roman_to_int(roman_string):
 
     values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     aux = []
+
     for i in roman_string:
-        aux.append(values[i])
+        if i in values:
+            aux.append(values[i])
 
     aux.append(0)
 
-    res = aux[0]
-    for j in range(1, len(aux) - 1):
+    res = 0
+    for j in range(0, len(aux) - 1):
 
         if aux[j + 1] > aux[j]:
-            res -= aux[j]
+            res = res - aux[j]
         else:
             res += aux[j]
     return(res)
