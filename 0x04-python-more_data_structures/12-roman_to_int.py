afaba146a85c2ca1a5d8bb026409bcd7171066e5
root <martin.liporace@gmail.com>
2022-05-04 10:27:11 -0700

    12-roman_to_int.py fixed again

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
 
-    if roman_string == None or type(roman_string) is not str:
+    if type(roman_string) is not str or roman_string is None:
         return(0)
     values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     aux = []
@@ -17,9 +17,7 @@ def roman_to_int(roman_string):
 
         if aux[j + 1] > aux[j] and aux[j] in validos:
             res = res - aux[j]
-    
-        elif aux[j + 1] > aux[j] and aux[j] not in validos:
-            return(0)
+
         else:
             res += aux[j]
     return(res)
