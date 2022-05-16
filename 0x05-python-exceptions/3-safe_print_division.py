aad673393b9ed5e89e2b8c051698fd52596d814f
root <martin.liporace@gmail.com>
2022-05-16 05:42:16 -0700

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+
+    try:
+        res = a/b
+
+    except (TypeError, ValueError, ZeroDivisionError):
+        res = "None"
+
+    finally:
+        print("Inside result: {}".format(res), end="")
+
+    print()
+    return res
