1c705a701959c01f4047976947c642d1f3f83697
root <martin.liporace@gmail.com>
2022-05-16 13:38:09 -0700

    100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as x:
+        return False
+        print("Exception:", x, file=sys.stderr)
