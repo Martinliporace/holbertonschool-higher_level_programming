a06450ac2c4cdde1d8b9e4999384c39c35e98076
root <martin.liporace@gmail.com>
2022-05-16 13:40:51 -0700

    100-safe_print_integer_err.py fxd

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as x:
-        return False
         print("Exception:", x, file=sys.stderr)
+        return False
