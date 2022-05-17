1326e23513b17bd5cb0c6f3fd6e6b3c2babdb8e9
root <martin.liporace@gmail.com>
2022-05-17 16:56:22 -0700

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,10 +6,7 @@ class Square:
 
     def __init__(self, size=0):
 
-        """size must be an integer, otherwise raise a
-        TypeError exception with the message size must be an integer
-        If size is less than 0, raise a ValueError exception
-        with the message size must be >= 0"""
+        """square instance"""
         self.size = size
 
     def area(self):
@@ -20,10 +17,12 @@ class Square:
 
     @property
     def size(self):
+        """retrieves size"""
         return(self.__size)
 
     @size.setter
     def size(self, value):
+        """sets size"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
 
