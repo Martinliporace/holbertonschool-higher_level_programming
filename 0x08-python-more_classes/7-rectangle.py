b10a8073678f86cc2d73b5d34fb02b3b78a19aad
root <martin.liporace@gmail.com>
2022-05-18 15:41:00 -0700

    7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -26,10 +26,10 @@ class Rectangle:
         """sets height"""
 
         if type(value) is not int:
-            raise TypeError("heightmust be an integer")
+            raise TypeError("height must be an integer")
 
         elif value < 0:
-            raise ValueError("heightmust be >= 0")
+            raise ValueError("height must be >= 0")
 
         else:
             self.__height = value
