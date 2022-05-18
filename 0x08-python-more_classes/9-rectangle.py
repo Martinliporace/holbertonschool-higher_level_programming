410ec943cccf225f2b5b588ec4ec050bd74d7680
root <martin.liporace@gmail.com>
2022-05-18 16:43:25 -0700

    9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -90,6 +90,7 @@ class Rectangle:
         print('Bye rectangle...')
         Rectangle.number_of_instances -= 1
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """returns the biggest rectangle based on the area"""
 
