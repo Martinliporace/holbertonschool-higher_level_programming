d160d85b30eca7e8a7fcbee5617b002bea4d04e2
root <martin.liporace@gmail.com>
2022-05-18 05:22:38 -0700

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Rectangle:
+
+    """Instantiation with optional width and height"""
+
+    def __init__(self, width=0, height=0):
+        """rectangle instance"""
+
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        """retrieves height"""
+        return(self.__height)
+
+    @height.setter
+    def height(self, value):
+        """sets height"""
+
+        if type(value) is not int:
+            raise TypeError("heightmust be an integer")
+
+        elif value < 0:
+            raise ValueError("heightmust be >= 0")
+
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        """retrieves width"""
+        return(self.__width)
+
+    @width.setter
+    def width(self, value):
+        """sets width"""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+        else:
+            self.__width = value
