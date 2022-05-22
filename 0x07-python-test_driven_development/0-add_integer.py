de440d0c9086f4c328538b796e87c66af3423f57
root <martin.liporace@gmail.com>
2022-05-22 06:31:42 -0700

    0-add_integer.py pcs fxd

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,6 +7,7 @@ a and b must be first casted to integers if they are float/
 Returns an integer: the addition of a and b
 """
 
+
 def add_integer(a, b=98):
     """function that adds 2 integers."""
 
@@ -16,9 +17,9 @@ def add_integer(a, b=98):
         b = int(b)
 
     if not isinstance(a, int) and not isinstance(a, float):
-            raise TypeError('a must be an integer')
+        raise TypeError('a must be an integer')
 
     if not isinstance(b, int) and not isinstance(b, float):
-            raise TypeError('b must be an integer')
+        raise TypeError('b must be an integer')
 
     return a + b
