4a3c65cc332f3c44f4a58c5c86ec1e482b917a3a
root <martin.liporace@gmail.com>
2022-05-23 06:56:39 -0700

    tests + 6

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+
+    """test for positive numbers"""
+    def test_PositiveNumbers(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    """test for disorder list"""
+    def test_DisorderList(self):
+        self.assertEqual(max_integer([8, 2, 9, 120]), 120)
+
+    """test for negative numbers"""
+    def test_NegativeNumbers(self):
+        self.assertEqual(max_integer([-8, -2, -9, -120]), -2)
+
+    """test for one +number"""
+    def test_OnePNumber(self):
+        self.assertEqual(max_integer([8]), 8)
+
+    """test for one -number"""
+    def test_OneNNumber(self):
+        self.assertEqual(max_integer([-9]), -9)
+
+    """test for empty list"""
+    def test_EmptyList(self):
+        self.assertEqual(max_integer([]), None)
+
+    """test for same values"""
+    def test_SameValues(self):
+        self.assertEqual(max_integer([8, 8, 8, 8]), 8)
+
+    """test for same and different values"""
+    def test_SameAndDifferentVal(self):
+        self.assertEqual(max_integer([8, 120, 9, 120]), 120)
+
+    """test for str values"""
+    def test_Str(self):
+        with self.assertRaises(TypeError):
+            max_integer("A")
+
+    """test for float values"""
+    def test_Float(self):
+        with self.assertRaises(TypeError):
+            max_integer([3.5, 8.3])
+
+
+if __name__ == '__main__':
+    unittest.main()
