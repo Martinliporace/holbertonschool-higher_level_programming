17b15791140ef39847da76cacfb5d3efb2b53de4
root <martin.liporace@gmail.com>
2022-05-30 12:21:57 -0700

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Same class or inherit from"""
+
+
+def inherits_from(obj, a_class):
+
+    """returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class ; otherwise False."""
+
+    return(issubclass(type(obj), a_class) and type(obj) != a_class)
