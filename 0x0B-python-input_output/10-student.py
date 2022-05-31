f48053fbb144a5557ace238f3dbb8985ff7e6e27
root <martin.liporace@gmail.com>
2022-05-31 16:24:27 -0700

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Student to JSON with filter"""
+
+
+class Student:
+    """class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def __str__(self):
+        """str"""
+        err = 'self.first_name, self.last_name, self.age'
+        return "[Student] {} - {:d}".format(err)
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+
+        if attrs is not None:
+            newDict = {}
+            for i in attrs:
+                if type(i) != str:
+                    return self.__dict__
+
+            for j in attrs:
+                if j in self.__dict__:
+                    newDict[j] = self.__dict__[j]
+
+            return newDict
+
+        else:
+            return self.__dict__
