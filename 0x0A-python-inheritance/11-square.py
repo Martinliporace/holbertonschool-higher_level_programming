6f5cfddab48a762b8596b401db579849e5d1b217
root <martin.liporace@gmail.com>
2022-05-30 17:00:24 -0700

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class"""
+    def __init__(self, size):
+        """init"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """area"""
+        return(self.__size ** 2)
+
+    def __str__(self):
+        """str"""
+        return("[Square] {}/{}".format(self.__size, self.__size))
