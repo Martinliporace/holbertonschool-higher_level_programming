095149acad42340c53312ba60dbcae5ceab94d4a
root <martin.liporace@gmail.com>
2022-06-01 14:37:29 -0700

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+
+    triangle = []
+
+    for i in range(n):
+        triangle.append([1])
+        for j in range(len(triangle) - 1):
+            triangle[i].append(1)
+            if j != 0 and j != (n-1) and i != 0:
+                triangle[i][j] = ((triangle[i-1][j-1]) + (triangle[i-1][j]))
+
+    return(triangle)
