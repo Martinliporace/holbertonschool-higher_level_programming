14b3068c675bff30a7ae0f5e14ae08e9b3a45f75
root <martin.liporace@gmail.com>
2022-06-04 14:02:35 -0700

    models

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,10 +18,38 @@ class Square(Rectangle):
     def size(self, size):
         """ size setter """
 
-        self.height = size
         self.width = size
+        self.height = size
 
     def __str__(self):
         return ("[Square] ({}) {}/{} - {}"
                 .format(self.id, self.x, self.y,
                         self.size))
+
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute."""
+
+        if args and len(args) > 0:
+            self.id = args[0]
+            if len(args) > 1:
+                self.integer_validator("size", args[1])
+                self.size = args[1]
+            if len(args) > 2:
+                self.xy_validator("x", args[2])
+                self.x = args[2]
+            if len(args) > 3:
+                self.xy_validator("y", args[3])
+                self.y = args[3]
+
+        else:
+            if kwargs is not None and len(kwargs) > 0:
+                for key, value in kwargs.items():
+                    if key == 'id':
+                        self.id = value
+                    if key == 'size':
+                        self.integer_validator("size", value)
+                    if key == 'x':
+                        self.xy_validator("x", value)
+                    if key == 'y':
+                        self.xy_validator("y", value)
+                    setattr(self, key, value)
