985072e934ee3bc9598d2880349b92cd131765a5
root <martin.liporace@gmail.com>
2022-06-04 06:17:49 -0700

    models

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,3 +84,21 @@ class Rectangle(Base):
                                                        self.__x, self.__y,
                                                        self.__width,
                                                        self.__height))
+
+    def update(self, *args):
+        """assigns an argument to each attribute."""
+
+        if args and len(args) > 0:
+            self.id = args[0]
+            if len(args) > 1:
+                self.integer_validator("width", args[1])
+                self.__width = args[1]
+            if len(args) > 2:
+                self.integer_validator("height", args[2])
+                self.__height = args[2]
+            if len(args) > 3:
+                self.xy_validator("x", args[3])
+                self.__x = args[3]
+            if len(args) > 4:
+                self.xy_validator("y", args[4])
+                self.__y = args[4]
