7ff45bd15c9ef87dd94832a51120226ee53a3840
root <martin.liporace@gmail.com>
2022-06-04 14:18:03 -0700

    models

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -53,3 +53,8 @@ class Square(Rectangle):
                     if key == 'y':
                         self.xy_validator("y", value)
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        dic = {'id':self.id, 'size':self.size,
+               'x':self.x, 'y':self.y}
+        return(dic)
