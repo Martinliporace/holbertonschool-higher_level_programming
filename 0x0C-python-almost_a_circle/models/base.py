470a31e03dcbabb9a4b2866206eaa7e55eadf545
root <martin.liporace@gmail.com>
2022-06-04 14:33:47 -0700

    models

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Base class"""
-
+import json
 
 class Base:
     """This class will be the "base" of all other classes in this project.
@@ -17,4 +17,11 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation
+        of list_dictionaries"""
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+                return "[]"
+        return json.dumps(list_dictionaries)
