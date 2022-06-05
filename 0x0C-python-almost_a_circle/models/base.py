49c6db46b7c592f0ce65249bb97fc1e104c0dcdf
root <martin.liporace@gmail.com>
2022-06-05 08:04:52 -0700

    models

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,11 @@ class Base:
                 f.write(cls.to_json_string(j_list))
             else:
                 f.write(cls.to_json_string(j_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
