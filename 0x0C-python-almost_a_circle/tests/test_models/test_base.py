0f864aaf88b6b6dd8f550127f99a2c22296925ca
root <martin.liporace@gmail.com>
2022-06-07 07:00:18 -0700

    tests

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Unittest for base"""
+
+import unittest
+from models.base import Base
+
+
+class Test_base(unittest.TestCase):
+
+    """test id None"""
+    def test_id_not_None(self):
+        b1 = Base(89)
+        self.assertEqual(b1.id, 89)
+
+    def test_id_None(self):
+        b2 = Base()
+        self.assertEqual(b2.id, 1)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
