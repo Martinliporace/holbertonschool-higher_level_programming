e2deb3028eb80bb0ebfe58acfdc720cae2ff90dc
root <martin.liporace@gmail.com>
2022-06-08 13:25:27 -0700

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -12,14 +12,25 @@ class Test_base(unittest.TestCase):
         b1 = Base(89)
         self.assertEqual(b1.id, 89)
 
-    def test_id_not_None(self):
-        b1 = Base(90)
-        self.assertEqual(b1.id, 90)
-
-
     def test_id_None(self):
         b2 = Base()
         self.assertEqual(b2.id, 1)
+
+    def test_type_error_str(self):
+        b2 = Base("Holberton")
+        self.assertRaises(TypeError)
+
+    def test_type_error_float(self):
+        b2 = Base(3.5)
+        self.assertRaises(TypeError)
+
+    def test_type_error_empty_list(self):
+        b2 = Base([])
+        self.assertRaises(TypeError)
+
+    def test_type_error_list(self):
+        b2 = Base([1, 2, 68])
+        self.assertRaises(TypeError)
 
 
 
