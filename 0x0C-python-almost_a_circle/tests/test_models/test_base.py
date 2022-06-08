231f8959571f9aeb60fee94db657d5c52efd59f5
root <martin.liporace@gmail.com>
2022-06-08 15:22:26 -0700

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -7,14 +7,20 @@ from models.base import Base
 
 class Test_base(unittest.TestCase):
 
-    """test id None"""
-    def test_id_not_None(self):
+    """test id 89"""
+    def test_id_not_89(self):
         b1 = Base(89)
         self.assertEqual(b1.id, 89)
 
-    def test_id_None(self):
         b2 = Base()
         self.assertEqual(b2.id, 1)
+
+        b2 = Base(None)
+        self.assertEqual(b2.id, 2)
+
+    def test_str_id(self):
+        b = Base("Holberton")
+        self.assertEqual(b.id, "Holberton")
 
 if __name__ == '__main__':
     unittest.main()
