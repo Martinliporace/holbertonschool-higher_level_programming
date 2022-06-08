548cd159bfe54c8d63382b74cb714b54efa5f83a
root <martin.liporace@gmail.com>
2022-06-08 14:04:15 -0700

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -34,16 +34,5 @@ class Test_base(unittest.TestCase):
         self.assertRaises(TypeError)
 
 
-class TestCodeFormat(unittest.TestCase):
-    """pep8"""
-
-    def test_pep8_conformance(self):
-        """Test that we conform to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files('models/base.py')
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-
 if __name__ == '__main__':
     unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -8,11 +8,20 @@ from models.rectangle import Rectangle
 class Test_rectangle(unittest.TestCase):
 
 
-    """test width not int"""
+    """test width int"""
     def test_width_is_int(self):
         r1 = Rectangle(10, 2)
         self.assertEqual(r1.width, 10)
 
+    """test width not int"""
+    def test_width_not_int_str(self):
+        r1 = Rectangle("H", 2)
+        self.assertRaises(TypeError)
+
+    """test width not int"""
+    def test_width_not_int_flt(self):
+        r1 = Rectangle(1.5, 2)
+        self.assertRaises(TypeError)
 
 
 if __name__ == '__main__':
