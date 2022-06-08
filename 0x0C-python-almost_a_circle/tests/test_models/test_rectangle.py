0d9e24e6d1fd3e5ff21670fdcbed6bc8f8808f94
root <martin.liporace@gmail.com>
2022-06-08 14:33:39 -0700

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -12,16 +12,5 @@ class Test_rectangle(unittest.TestCase):
         r1 = Rectangle(10, 2)
         self.assertEqual(r1.width, 10)
 
-    """test width not int"""
-    def test_width_not_int_str(self):
-        r1 = Rectangle("H", 2)
-        self.assertRaises(TypeError)
-
-    """test width not int"""
-    def test_width_not_int_flt(self):
-        r1 = Rectangle(1.5, 2)
-        self.assertRaises(TypeError)
-
-
 if __name__ == '__main__':
     unittest.main()
