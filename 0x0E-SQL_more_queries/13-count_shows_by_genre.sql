32ef1038240d9c9ff5768ffecc48c5e6f22c90e6
root <martin.liporace@gmail.com>
2022-07-06 19:08:35 -0700

    13-count_shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Task 13. Number of shows by genre
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_show_genres.genre_id IS NOT NULL
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC
