befd133e7f6d931c0116b3f02b04f21f19eb62e5
root <martin.liporace@gmail.com>
2022-08-01 07:31:57 -0700

    semistd fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let myVar = 'C is fun';
-  console.log(myVar);
+console.log(myVar);
 myVar = 'Python is cool';
-  console.log(myVar);
+console.log(myVar);
 myVar = 'JavaScript is amazing';
-  console.log(myVar);
+console.log(myVar);
