b3d6b0d709979bcf76f69e0418643112697ec5f3
root <martin.liporace@gmail.com>
2022-08-02 08:00:56 -0700

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+    if (!w || !h || w <= 0 || h <= 0) {
+      return (Rectangle);
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) { console.log('X'.repeat(this.width)); }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
