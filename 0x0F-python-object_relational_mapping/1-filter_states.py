6f36d3a11bf8e02ac6368a048636ee521d2d60d0
root <martin.liporace@gmail.com>
2022-08-04 14:30:17 -0700

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Task: 0. Get all states"""
+
+import MySQLdb
+from sys import argv
+
+if (__name__ == "__main__"):
+
+    con = MySQLdb.Connect(host="localhost", user=argv[1],
+                          passwd=argv[2], db=argv[3], port=3306)
+
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    query = cur.fetchall()
+
+    for x in query:
+        print(x)
+    cur.close()
+    con.close()
