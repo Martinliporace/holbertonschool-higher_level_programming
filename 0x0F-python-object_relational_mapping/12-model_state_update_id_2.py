92ccadc523f9a30e36c3418b09203b87e95bdded
root <martin.liporace@gmail.com>
2022-08-06 11:46:18 -0700

    12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Task: 0. Get all states"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if (__name__ == "__main__"):
+
+    engine = create_engine('mysql://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    newValue = "New Mexico"
+    for state in session.query(State).order_by(State.id):
+        if state.id == 2:
+            state.name = newValue
+            session.commit()
+    session.close()
