334f3fb77ea5a84dabdba38b55905834fb8e6969
root <martin.liporace@gmail.com>
2022-08-06 12:18:13 -0700

    13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,7 +12,6 @@ if (__name__ == "__main__"):
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
 
-    
     Session = sessionmaker(bind=engine)
     session = Session()
     toDel = session.query(State).filter(State.name.like('%a%')).all()
