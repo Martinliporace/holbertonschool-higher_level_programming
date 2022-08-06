32049a52c36cbe73052750ff2543d797bd91403a
root <martin.liporace@gmail.com>
2022-08-06 13:17:03 -0700

    model_city.py

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Task: 0. Get all states"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
+
+
+class City(Base):
+    """el coso que va aca"""
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, primary_key=True, nullable=False,
+                autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
