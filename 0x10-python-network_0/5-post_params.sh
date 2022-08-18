f01cd662c809b7ba896d93e1efba2cc26e149fde
Martinliporace <98000916+Martinliporace@users.noreply.github.com>
2022-08-18 12:19:18 -0300

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Task 5. cURL POST parameters
-curl -F "email=test@gmail.com" -F "subject=I will always be here for PLD" $1
+curl -s -F "email=test@gmail.com" -F "subject=I will always be here for PLD" $1
