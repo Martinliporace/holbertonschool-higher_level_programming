264f991e2593e9484eb1cf4bd1053c4b61359c4e
martinliporace <martin.liporace@gmail.com>
2022-08-18 14:11:06 +0000

    fix format

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Task 5. cURL POST parameters
 
-curl -F "email=test@gmail.com&subject=I will always be here for PLD" $1
+curl -F "email=test@gmail.com" -F "subject=I will always be here for PLD" $1
