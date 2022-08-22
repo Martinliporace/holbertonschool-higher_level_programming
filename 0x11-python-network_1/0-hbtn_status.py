795aa268d7a683c0bce0c2a883ded6ad5822628a
martinliporace <martin.liporace@gmail.com>
2022-08-22 21:56:14 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Task 0. What's my status? #0"""
-from urllib.request import urlopen
+if __name__ == "__main__":
+    from urllib.request import urlopen
 
-with urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-print('Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'
-      .format(type(html), html, html.decode('utf-8')))
+    with urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+    print('Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'
+          .format(type(html), html, html.decode('utf-8')))
