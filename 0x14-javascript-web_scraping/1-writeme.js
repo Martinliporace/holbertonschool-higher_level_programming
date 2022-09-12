894670b11f3d8fb97f8f3c5f1367e89f56e425b9
martinliporace <martinliporace@gmail.com>
2022-09-12 09:53:45 -0700

    fix blankline

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,6 +3,6 @@
 const fs = require('fs');
 const file = process.argv[2];
 const text = process.argv[3];
-fs.writeFile(file, text + '\n', 'utf-8', error => {
+fs.writeFile(file, text, 'utf-8', error => {
   if (error) { console.log(error); }
 });
