0442deb0737e1a50893ae7b429c7b0023115acb4
martinliporace <martinliporace@gmail.com>
2022-09-12 10:57:47 -0700

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const axios = require('axios').default;
+const url = process.argv[2];
+let cont = 0;
+let total = 0;
+axios.get(url)
+  .then(datos => {
+    total = datos.data.count;
+    for (let i = 0; i < total; i++) {
+      const chars = datos.data.results[i];
+      const cant = chars.characters.length;
+      for (let j = 0; j < cant; j++) {
+        if (chars.characters[j] === 'https://swapi-api.hbtn.io/api/people/18/') {
+          cont++;
+        }
+      }
+    }
+    console.log(cont);
+  });
