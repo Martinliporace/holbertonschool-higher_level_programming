b1b47f113575d2fc8d4d4bac1c729b8c73a1256c
martinliporace <martin.liporace@gmail.com>
2022-09-19 13:26:34 +0000

    i like this method because is better

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,15 +1,8 @@
 $(document).ready(function(){
     $("#toggle_header").on({
         click: function(){
-            const clase = ($('header').attr('class'));
-            console.log(clase);
-            if(clase === 'green'){
-                $('header').removeClass("green");
-                $('header').addClass("red");
-            }else{
-                $('header').removeClass("red");
-                $('header').addClass("green");
-            }
+            $('header').toggleClass('red');
+            $('header').toggleClass('green');
         }
-      });
-});
+})
+})
